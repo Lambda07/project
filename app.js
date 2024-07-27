10569a931260853d7717d0dceeb4f134320d8f09
Lambda07 <155990728+Lambda07@users.noreply.github.com>
2024-07-27 20:05:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./server/router');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const path = require('path');
+const http = require('http');
+const server = http.createServer(app);
+const socketIo = require('socket.io');
+const io = new socketIo.Server(server);
+const formatMessage = require('./server/utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers, giveAllRooms, createRoom} = require('./server/utils/users');
+//****End of imports****
+// dbgFx8wj5pezQP0S
+
+const connection = () => {
+    mongoose.connect('mongodb+srv://shivamdotsingh:dbgFx8wj5pezQP0S@cluster0.n51ujuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('Connected With Mongo DB');
+};
+
+connection();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}));
+
+app.use(cookieParser());
+
+app.use(express.static(path.resolve(__dirname, './public')));
+const botName = 'Cody';
+
+io.on('connection', socket => {
+    console.log('a user connected');
+    socket.on('joinRoom', ({
+        username, room
+    }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+    //    welcome current user
+        socket.emit('messageFromServer', formatMessage(botName, 'Welcome To ChatPRO'));
+    //    broadcast when a user enters
+        socket.broadcast
+            .to(user.room)
+            .emit('messageFromServer', formatMessage(botName, String(`${user.userName} has joined the room`)));
+
+    //    send users and room info
+        io
+            .to(user.room)
+            .emit('roomUser', {
+                roomNameSent: String(user.room),
+                users : getRoomUsers(user.room)
+            })
+    })
+
+//    listen to chat messages
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('messageFromServer', formatMessage(user.userName, msg))
+    })
+
+    //message to notify user disconnection
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if(user) {
+            io.to(user.room).emit('messageFromServer', formatMessage(botName, `${user.userName} has left the room`));
+            io.to(user.room).emit('roomUser', {roomNameSent : String(user.room), users : getRoomUsers(user.room)});
+        }
+    })
+})
+
+app.use('/', router);
+//********* listening to port ********
+server.listen(3000, () => {
+    console.log('connecting to http://localhost:3000');
+});
---
+++ server/router.js
@@ -0,0 +1,463 @@
+const express = require('express');
+const router = express.Router();
+const schema = require('./users');
+const otherSchema = require('./otherSchema');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const { send } = require('express/lib/response');
+const { doc } = require('./users');
+
+var session;
+
+router.get('/', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '1'){
+            res.redirect('/doctor');
+        }else if(session.type == '2'){
+            res.redirect('/family');
+        }else if(session.type == '3'){
+            res.redirect('/patient');
+        }
+    }else{
+        res.render('index');
+    }
+})
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+router.get('/signUp', (req, res) => {
+    schema.doc.find().then(data => {
+        res.render('signup', {doctorsList : data})
+    }).catch(err => {
+        res.send(err);
+    })
+})
+router.post('/login', (req, res) => {
+    if(req.body.typeAcc == '1'){
+        schema.doc.find({email : req.body.email, password: req.body.password}).then(data => {
+            session=req.session;
+            session.userid=data[0]._id;
+            session.type = '1';
+            res.redirect('/');
+        }).catch(err => {
+            res.send(err);
+        })
+    }else if(req.body.typeAcc == '2'){
+        schema.fam.find({email : req.body.email, password: req.body.password}).then(data => {
+            session=req.session;
+            session.userid=data[0]._id;
+            session.type = '2';
+            res.redirect('/');
+        }).catch(err => {
+            res.send(err);
+        })
+    }else if(req.body.typeAcc == '3'){
+        schema.pat.find({email : req.body.email, password: req.body.password}).then(data => {
+            session=req.session;
+            session.userid=data[0]._id;
+            session.type = '3';
+            res.redirect('/');
+        }).catch(err => {
+            res.send(err);
+        })
+    }
+})
+router.post('/signUp', (req, res) => {
+    console.log(req.body)
+    if(req.body.typeAcc === '1'){
+        const addData = new schema.doc({
+            typeAcc : req.body.typeAcc,
+            name : req.body.firstName + " " + req.body.lastName,
+            email : req.body.email,
+            PhoneNo : req.body.phoneNo,
+            covidStatus : req.body.covidStat === 'true',
+            address : req.body.address,
+            health : req.body.healthInfo,
+            freeTimeStart : new Date().setHours(req.body.InputStartFreeTime.slice(0,2), req.body.InputStartFreeTime.slice(3)),
+            freeTimeEnd : new Date().setHours(req.body.InputEndFreeTime.slice(0,2), req.body.InputEndFreeTime.slice(3)),
+            password : req.body.password
+        }).save().catch(err => {
+            res.send(err);
+        }).then(data => {
+            session=req.session;
+            session.userid=data._id;
+            session.type = '1';
+            res.redirect(`/`);
+        })
+    }else if(req.body.typeAcc === '2'){
+        const addData = new schema.fam({
+            typeAcc : req.body.typeAcc,
+            name : req.body.firstName + " " + req.body.lastName,
+            email : req.body.email,
+            PhoneNo : req.body.phoneNo,
+            covidStatus : req.body.covidStat === 'true',
+            address : req.body.address,
+            health : req.body.healthInfo,
+            password : req.body.password,
+            relatedDoc : req.body.InputRelatedDoc
+        }).save().catch(err => {
+            res.send(err);
+        }).then(data => {
+            session=req.session;
+            session.userid=data._id;
+            session.type = '2';
+            res.redirect(`/`);
+        })
+    }else if(req.body.typeAcc === '3'){
+        const addData = new schema.pat({
+            typeAcc : req.body.typeAcc,
+            name : req.body.firstName + " " + req.body.lastName,
+            email : req.body.email,
+            PhoneNo : req.body.phoneNo,
+            covidStatus : req.body.covidStat === 'true',
+            address : req.body.address,
+            health : req.body.healthInfo,
+            password : req.body.password,
+        }).save().catch(err => {
+            res.send(err);
+        }).then(data => {
+            session=req.session;
+            session.userid=data._id;
+            session.type = '3';
+            res.redirect(`/`);
+        })
+    }
+})
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
+
+router.get('/doctor', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '1'){
+            schema.doc.findById(session.userid).then(data => {
+                otherSchema.help.find().then(helpThem => {
+                    res.render('dashboardMain', {data : data, helpThem : helpThem});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/');
+    }
+})
+router.get('/patient', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '3'){
+            schema.pat.findById(session.userid).then(data => {
+                otherSchema.help.find().then(helpThem => {
+                    res.render('dashboardMain', {data : data, helpThem : helpThem});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/');
+    }
+})
+router.get('/family', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '2'){
+            schema.fam.findById(session.userid).then(data => {
+                otherSchema.help.find().then(helpThem => {
+                    res.render('dashboardMain', {data : data, helpThem : helpThem});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/');
+    }
+})
+
+router.get('/blog-room', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '1'){
+            schema.doc.findById(session.userid).then(data => {
+                schema.fam.find({relatedDoc : data._id}).then(family => {
+                    res.render('family-stat', {data : data, family : family});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }).catch(err => {
+                res.send(err);
+            })
+        }else if(session.type == '2'){
+            schema.fam.findById(session.userid).then(data => {
+                schema.fam.find({relatedDoc : data.relatedDoc}).then(family => {
+                    schema.doc.find({_id : data.relatedDoc}).then(familyDoc => { 
+                        res.render('family-stat', {data : data, family : family, familyDoc: familyDoc});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }).catch(err => {
+                    res.send(err);
+                })
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/');
+    }
+})
+
+router.get('/help-a-person/:id', (req, res) => {
+    otherSchema.help.findById(req.params.id).then(helpData => {
+        session=req.session;
+            if(session.userid){
+                if(session.type == '1'){
+                    schema.doc.findById(session.userid).then(data => {
+                        res.render('help-show', {data : data, helpData:helpData});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '2'){
+                    schema.fam.findById(session.userid).then(data => {
+                        res.render('help-show', {data : data, helpData:helpData});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '3'){
+                    schema.pat.findById(session.userid).then(data => {
+                        res.render('help-show', {data : data, helpData:helpData});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else{
+                    res.redirect('/');
+                }
+            }else{
+                res.redirect('/');
+            }
+    }).catch(err => {res.send(err);})
+})
+router.post('/help-a-person/:id', (req, res) => {
+    otherSchema.help.findByIdAndDelete(req.params.id).catch(err => {res.send(err)});
+    res.redirect('/')
+})
+router.get('/ask-for-help', (req, res) => {
+    otherSchema.help.find().then(data => {
+        if(data.length < 9){
+            session=req.session;
+            if(session.userid){
+                if(session.type == '1'){
+                    schema.doc.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: true});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '2'){
+                    schema.fam.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: true});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '3'){
+                    schema.pat.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: true});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else{
+                    res.redirect('/');
+                }
+            }else{
+                res.redirect('/');
+            }
+        }else{
+            session=req.session;
+            if(session.userid){
+                if(session.type == '1'){
+                    schema.doc.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: false});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '2'){
+                    schema.fam.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: false});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else if(session.type == '3'){
+                    schema.pat.findById(session.userid).then(data => {
+                        res.render('ask-help', {data : data, canAsk: false});
+                    }).catch(err => {
+                        res.send(err);
+                    })
+                }else{
+                    res.redirect('/');
+                }
+            }else{
+                res.redirect('/');
+            }
+        }
+    })
+})
+router.post('/ask-for-help', (req, res) => {
+    const addData = new otherSchema.help({
+        typeAcc : req.body.userType,
+        name : req.body.name,
+        email : req.body.email,
+        PhoneNo : req.body.phoneNo,
+        userId : req.body.userId,
+        need : req.body.need,
+        explanation : req.body.explanation
+    }).save().catch(err => {
+        res.send(err);
+    }).then(data => {
+        res.redirect(`/`);
+    })
+})
+router.get('/consult-a-doctor', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '3'){
+            schema.pat.findById(session.userid).then(data => {
+                schema.doc.find().then(dataDoc => {
+                    let dataDocArr = []
+                    dataDoc.forEach(function(docData){
+                        obj = docData;
+                        obj.password = undefined;
+                        obj.typeAcc = undefined;
+                        obj.covidStatus = undefined;
+                        obj.address = undefined;
+                        obj.health = undefined;
+                        obj.freeTimeStart = undefined;
+                        obj.freeTimeEnd = undefined;
+                        dataDocArr.push(obj)
+                    })
+                    res.render('consult-docs', {data : data, dataDocs: dataDocArr});
+                }).catch(err => {res.send(err)})
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/');
+    }
+})
+router.get('/profile/:typeAccount/:id', (req, res) => {
+        session=req.session;
+        if(session.userid){
+            if(req.params.typeAccount == '1'){
+                schema.doc.findById(req.params.id).then(data => {
+                    let x = false;
+                    if(data._id == session.userid){
+                        x = true;
+                    }
+                    res.render('profile', {data : data, yourAcc : x});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }else if(req.params.typeAccount == '2'){
+                schema.fam.findById(req.params.id).then(data => {
+                    let x = false;
+                    if(data._id == session.userid){
+                        x = true;
+                    }
+                    res.render('profile', {data : data, yourAcc : x});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }else if(req.params.typeAccount == '3'){
+                schema.pat.findById(req.params.id).then(data => {
+                    let x = false;
+                    if(data._id == session.userid){
+                        x = true;
+                    }
+                    res.render('profile', {data : data, yourAcc : x});
+                }).catch(err => {
+                    res.send(err);
+                })
+            }else{
+                res.redirect('/');
+            }
+        }else{
+            res.redirect('/');
+        }
+})
+router.get('/edit-profile', (req, res) => {
+    session=req.session;
+    if(session.userid){
+        if(session.type == '1'){
+            schema.doc.findById(session.userid).then(data => {
+                res.render('edit-profile', {data : data});
+            }).catch(err => {
+                res.send(err);
+            })
+        }else if(session.type == '2'){
+            schema.fam.findById(session.userid).then(data => {
+                res.render('edit-profile', {data : data});
+            }).catch(err => {
+                res.send(err);
+            })
+        }else if(session.type == '3'){
+            schema.pat.findById(session.userid).then(data => {
+                res.render('edit-profile', {data : data});
+            }).catch(err => {
+                res.send(err);
+            })
+        }else{
+            res.redirect('/');
+        }
+    }else{
+        res.redirect('/')
+    }
+})
+router.post('/edit-profile', (req, res) => {
+    dat = req.body;
+    session = req.session;
+    if(session.type == '1'){
+        console.log(dat);
+        dat.freeTimeStart = new Date().setHours(dat.freeTimeStart.slice(0,2), dat.freeTimeStart.slice(3));
+        dat.freeTimeEnd = new Date().setHours(dat.freeTimeEnd.slice(0,2), dat.freeTimeEnd.slice(3));
+        console.log(dat.freeTimeStart);
+        schema.doc.findOneAndUpdate({_id : session.userid}, dat).then(data => {
+            res.redirect('/')
+        }).catch(err => res.send(err))
+    }else if(session.type == '2'){
+        schema.fam.findOneAndUpdate({_id : session.userid}, dat).then(data => {
+            res.redirect('/')
+        }).catch(err => res.send(err))
+    }else if(session.type == '3'){
+        schema.pat.findOneAndUpdate({_id : session.userid}, dat).then(data => {
+            res.redirect('/')
+        }).catch(err => res.send(err))
+    }else{
+        res.redirect('/')
+    }
+})
+
+router.get('/chat/:roomType', (req, res) => {
+    res.render('chat');
+})
+
+module.exports = router;
